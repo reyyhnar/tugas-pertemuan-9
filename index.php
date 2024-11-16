80da05971bdadbdb85b6e5b518e821e0d8153aca
Reyhana Alya Roren <reyhana71005@gmail.com>
2024-11-16 12:32:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+$nama = "Reyhana Alya Roren";
+$kelas= "TI.23.CA.1";
+$nim = "312310089";
+$prodi = "Teknik Informatika";
+
+ ?>
+
+ <h1>
+ 	Saya Adalah
+ 	<?php echo $nama; ?>
+ </h1>
+ <p>
+ 	<strong>Kelas</strong>=<?php echo $kelas; ?>
+ </p>
+ <p>
+ 	<strong>Nim</strong>=<?php echo $nim; ?>
+ </p>
+ <p>
+ 	<strong>Prodi</strong>=<?php echo $prodi; ?>
+ </p>
